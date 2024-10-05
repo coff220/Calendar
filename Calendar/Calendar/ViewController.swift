56cab90433ddae1a34d580a6f41aa51a7b811d40
Viacheslav <viacheslav@Viacheslavs-MacBook-Air.local>
2024-10-05 15:26:27 +0200

    begin poject

---
+++ Calendar/Calendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Viacheslav on 05/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
