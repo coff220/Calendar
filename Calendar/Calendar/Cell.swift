1f2f082124a56b6fc1d04daf811e09d8e6a3fd96
Viacheslav <viacheslav@Viacheslavs-MacBook-Air.local>
2024-10-11 17:31:03 +0200

    ui calendar

---
+++ Calendar/Calendar/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  Calendar
+//
+//  Created by Viacheslav on 07/10/2024.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+}
